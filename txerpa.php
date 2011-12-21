91fc28cee38d739fad00c7b6f40e788735b3dc2b
Hugo Chinchilla Carbonell <hugo@bulma.net>
2011-12-21 15:17:59 +0100

    Added curl as a submodule

--- txerpa.php
+++ txerpa.php
@@ -1,5 +1,5 @@
 <?php
 
-require_once 'curl.php';
+require_once 'curl/curl.php';
 require_once 'lib'.DIRECTORY_SEPARATOR.'txerpa.php';
 require_once 'lib'.DIRECTORY_SEPARATOR.'txerpa_exception.php';
